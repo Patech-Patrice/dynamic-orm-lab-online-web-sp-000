039cba2912d5fd4cc58e6d6f1be1a9a8810f8d21
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-12 18:23:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,11 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-  self.column_names.each do |attribute_name|
-    attr_accessor attribute_name.to_sym
-end
-end
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+    end
+  end
+end  
+  
+	
+
